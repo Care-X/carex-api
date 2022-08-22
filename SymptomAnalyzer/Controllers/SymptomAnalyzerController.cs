0046fade8dcd5a02510fdcf21c5b10267b8115d2
sathsarabandaraj <sathsarabjayasundara@gmail.com>
2022-08-22 23:45:07 +0530

    Add project files.

---
+++ SymptomAnalyzer/Controllers/SymptomAnalyzerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML;
+using SymptomAnalyzer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SymptomAnalyzer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SymptomAnalyzerController : ControllerBase
+    {
+        public static readonly Lazy<PredictionEngine<SymptomModel, PreditionModel>> PredictEngine = new(() => CreatePredictEngine(), true);
+
+        [Route("predict")]
+        [HttpPost]
+        public PreditionModel Predict(SymptomModel input)
+        {
+            var predEngine = PredictEngine.Value;
+            return predEngine.Predict(input);
+        }
+
+        private static PredictionEngine<SymptomModel, PreditionModel> CreatePredictEngine()
+        {
+            var mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load("./Assets/MLModel.zip", out var _);
+            return mlContext.Model.CreatePredictionEngine<SymptomModel, PreditionModel>(mlModel);
+        }
+    }
+}
---
+++ SymptomAnalyzer/Models/PreditionModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML.Data;
+
+namespace SymptomAnalyzer.Models
+{
+    public class PreditionModel
+    {
+        [ColumnName("PredictedLabel")]
+        public string? Prediction { get; set; }
+
+        public float[]? Score { get; set; }
+    }
+}
