0247b67f8cbaaae0233641971267d723634714a3
Sathsara Bandara Jayasundara <62067185+sathsarabandaraj@users.noreply.github.com>
2022-10-31 22:38:16 +0530

    Verify user (#4)
    * Add firebase secrets file path to .gitignore
    
    * Uncomment "VerifyUserAsync"

--- SymptomAnalyzer/FirestoreFunctions.cs
+++ SymptomAnalyzer/FirestoreFunctions.cs
@@ -1,24 +1,25 @@
-﻿//using Google.Cloud.Firestore;
-//using SymptomAnalyzer.Models;
+﻿using consosleapp;
+using Google.Cloud.Firestore;
+using SymptomAnalyzer.Models;
 
-//namespace SymptomAnalyzer.Helper
-//{
-//    public class FirestoreFunctions
-//    {
-//        public readonly static FirestoreDb db = FirestoreDb.Create("carex-87914");
+namespace SymptomAnalyzer.Helper
+{
+    public class FirestoreFunctions
+    {
+        public readonly static FirestoreDb db = FirestoreDb.Create("carex-87914");
 
-//        public static async Task<FirebaseUserModel> VerifyUserAsync()
-//        {
-//            DocumentReference docRef = db.Collection("UserData").Document("Qxhmru4Jcegwqakdp1SBR0HUckK2");
-//            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
-//            if (snapshot.Exists)
-//            {
-//                return snapshot.ConvertTo<FirebaseUserModel>();
-//            }
-//            else
-//            {
-//                return new FirebaseUserModel();
-//            }
-//        }
-//    }
-//}
+        public static async Task<firebaseusermodel> VerifyUserAsync()
+        {
+            DocumentReference docRef = db.Collection("UserData").Document("3fKTS8Lk44MBGHBpyZa6KeeQTxd2");
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+            if (snapshot.Exists)
+            {
+                return snapshot.ConvertTo<firebaseusermodel>();
+            }
+            else
+            {
+                return new firebaseusermodel();
+            }
+        }
+    }
+}
--- SymptomAnalyzer/Models/FirebaseUserModel.cs
+++ SymptomAnalyzer/Models/FirebaseUserModel.cs
@@ -1,13 +1,12 @@
-﻿//using Microsoft.ML.Data;
-
-//namespace ConsosleApp
-//{
-//    public class FirebaseUserModel
-//    {
-//        public string b { get; set; }
-//        public string e { get; set; }
-//        public string f { get; set; }
-//        public string m { get; set; }
-//        public string s { get; set; }
-//    }
-//}
+﻿
+namespace consosleapp
+{
+    public class firebaseusermodel
+    {
+        public string b { get; set; }
+        public string e { get; set; }
+        public string f { get; set; }
+        public string m { get; set; }
+        public string s { get; set; }
+    }
+}
