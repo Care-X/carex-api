269d93b1f56f5dd7ce93b5dcfc8b88647f7b59de
Sathsara Bandara Jayasundara <sathsarabjayasundara@gmail.com>
2022-10-31 22:45:44 +0530

    Extend JWT key expire time

--- SymptomAnalyzer/FirestoreFunctions.cs
+++ SymptomAnalyzer/FirestoreFunctions.cs
@@ -1,25 +1,25 @@
-﻿using consosleapp;
-using Google.Cloud.Firestore;
-using SymptomAnalyzer.Models;
+﻿//using consosleapp;
+//using Google.Cloud.Firestore;
+//using SymptomAnalyzer.Models;
 
-namespace SymptomAnalyzer.Helper
-{
-    public class FirestoreFunctions
-    {
-        public readonly static FirestoreDb db = FirestoreDb.Create("carex-87914");
+//namespace SymptomAnalyzer.Helper
+//{
+//    public class FirestoreFunctions
+//    {
+//        public readonly static FirestoreDb db = FirestoreDb.Create("carex-87914");
 
-        public static async Task<firebaseusermodel> VerifyUserAsync()
-        {
-            DocumentReference docRef = db.Collection("UserData").Document("3fKTS8Lk44MBGHBpyZa6KeeQTxd2");
-            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
-            if (snapshot.Exists)
-            {
-                return snapshot.ConvertTo<firebaseusermodel>();
-            }
-            else
-            {
-                return new firebaseusermodel();
-            }
-        }
-    }
-}
+//        public static async Task<firebaseusermodel> VerifyUserAsync()
+//        {
+//            DocumentReference docRef = db.Collection("UserData").Document("3fKTS8Lk44MBGHBpyZa6KeeQTxd2");
+//            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+//            if (snapshot.Exists)
+//            {
+//                return snapshot.ConvertTo<firebaseusermodel>();
+//            }
+//            else
+//            {
+//                return new firebaseusermodel();
+//            }
+//        }
+//    }
+//}
